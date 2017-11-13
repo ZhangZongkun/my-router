45e23c84920c80a84a973958dfcbfb121c351dcd
Zongkun <nkzongkun@163.com>
2017-11-13 18:36:05 +0800

    增加AwesomePipe和HighlightDirective

---
+++ src/app/shared/highlight.directive.ts
@@ -0,0 +1,8 @@
+import { Directive, ElementRef } from '@angular/core';
+@Directive({selector: '[highlight], input'})
+export class HighlightDirective {
+  constructor(el: ElementRef) {
+    el.nativeElement.style.backgroundColor = 'lightgray';
+    console.log(`* Shared highlight called for ${el.nativeElement.tagName}`);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { AwesomePipe } from './awesome.pipe';
+import { HighlightDirective } from './highlight.directive';
+
 @NgModule({
   imports: [CommonModule],
-  declarations: [],
-  exports: [CommonModule, FormsModule]
+  declarations: [AwesomePipe, HighlightDirective],
+  exports: [CommonModule, FormsModule, AwesomePipe, HighlightDirective]
 })
 export class SharedModule {
 }
