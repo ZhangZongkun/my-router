9bcd5b7067b94f0aa26d89ea5e476f7955f627a2
Zongkun <nkzongkun@163.com>
2017-11-18 12:51:35 +0800

    稍作修改

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { heroes } from './hero';
+
 @Component({
   templateUrl: './example.component.html'
 })
 export class ExampleComponent {
   color: string;
+  heroes = heroes;
+  hero = this.heroes[0];
 
   everyFiveSeconds() {
     console.log('Every five seconds.');
--- src/app/example/example.module.ts
+++ src/app/example/example.module.ts
@@ -6,10 +6,12 @@ import { ExampleComponent } from './example.component';
 import { BankComponent } from './bank.component';
 import { IntervalDirective } from './interval.directive';
 import { HighlightDirective } from './highlight.directive';
+import { heroSwitchComponents } from './hero-switch.components';
 
 @NgModule({
   imports: [CommonModule, ExampleRoutingModule],
-  declarations: [ExampleComponent, BankComponent, IntervalDirective, HighlightDirective]
+  declarations: [ExampleComponent, BankComponent, IntervalDirective, HighlightDirective,
+    heroSwitchComponents]
 })
 export class ExampleModule {
 }
