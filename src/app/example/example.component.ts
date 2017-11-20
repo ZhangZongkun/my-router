e3e58e67cf759f6deee9fea25ef5236a84ed18e7
Zongkun <nkzongkun@163.com>
2017-11-20 18:16:49 +0800

    增加UnlessDirective

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { heroes } from './hero';
 
 @Component({
-  templateUrl: './example.component.html'
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css']
 })
 export class ExampleComponent {
   color: string;
   heroes = heroes;
   hero = this.heroes[2];
   showSad = true;
+  condition = false;
 
   everyFiveSeconds() {
     console.log('Every five seconds.');
--- src/app/example/example.module.ts
+++ src/app/example/example.module.ts
@@ -4,13 +4,15 @@ import { SharedModule } from '../shared/shared.module';
 import { ExampleRoutingModule } from './example-routing.module';
 import { ExampleComponent } from './example.component';
 import { BankComponent } from './bank.component';
-import { IntervalDirective } from './interval.directive';
 import { heroSwitchComponents } from './hero-switch.components';
+
+import { IntervalDirective } from './interval.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   imports: [SharedModule, ExampleRoutingModule],
-  declarations: [ExampleComponent, BankComponent, IntervalDirective,
-    heroSwitchComponents]
+  declarations: [ExampleComponent, BankComponent, heroSwitchComponents,
+    IntervalDirective, UnlessDirective]
 })
 export class ExampleModule {
 }
