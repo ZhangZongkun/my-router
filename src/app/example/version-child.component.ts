09627bfd43b763f4da5239a55113312313aeae39
Zongkun <nkzongkun@163.com>
2017-12-03 12:27:50 +0800

    稍作修改

--- src/app/example/version-child.component.ts
+++ src/app/example/version-child.component.ts
@@ -4,7 +4,10 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
   selector: 'app-version-child',
   template: `
     <h3>Version {{major}}.{{minor}}</h3>
-    <h4>Change log: </h4>
+    <h4>Change log:</h4>
+    <ul>
+      <li *ngFor="let change of changeLog">{{change}}</li>
+    </ul>
   `
 })
 export class VersionChildComponent implements OnChanges {
@@ -14,5 +17,17 @@ export class VersionChildComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     // console.log(changes);
+    let log: string[] = [];
+    for (let propName of Object.keys(changes)) {
+      let changedProp = changes[propName];
+      let to = JSON.stringify(changedProp.currentValue);
+      if (changedProp.isFirstChange()) {
+        log.push(`Initial value of ${propName} set to ${to}`);
+      } else {
+        let from = changedProp.previousValue;
+        log.push(`${propName} changed from ${from} to ${to}`);
+      }
+    }
+    this.changeLog.push(log.join(', '));
   }
 }
