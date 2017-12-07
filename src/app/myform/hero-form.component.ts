e4ba4019907da1cc7dc3b62937dd1c539a719c44
Zongkun <nkzongkun@163.com>
2017-12-07 19:05:09 +0800

    稍作修改

--- src/app/myform/hero-form.component.ts
+++ src/app/myform/hero-form.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './Hero';
+
 @Component({
   selector: 'app-hero-form',
-  template: `<p>Great, I'm good!</p>`
+  templateUrl: './hero-form.component.html'
 })
 export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chunk Overstreet');
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
 }
