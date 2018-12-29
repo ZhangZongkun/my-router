adda56315f685cd600fdbd952b9b4d53f06654c0
zongkun <nkzongkun@163.com>
2018-12-29 17:08:32 +0800

    使用ngx-bootstrap

--- src/app/crisis/crisis.module.ts
+++ src/app/crisis/crisis.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { CrisisRoutingModule } from './crisis-routing.module';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { CrisisDetailComponent } from './crisis-detail.component';
 import { CrisisService } from './crisis.service';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, CrisisRoutingModule],
+  imports: [SharedModule, CrisisRoutingModule],
   declarations: [CrisisCenterComponent, CrisisListComponent, CrisisDetailComponent, CrisisCenterComponent],
   providers: [CrisisService]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AwesomePipe } from './awesome.pipe';
 import { HighlightDirective } from './highlight.directive';
 
+import { AlertModule } from 'ngx-bootstrap/alert';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AlertModule.forRoot()],
   declarations: [AwesomePipe, HighlightDirective],
-  exports: [CommonModule, FormsModule, AwesomePipe, HighlightDirective]
+  exports: [CommonModule, AlertModule, FormsModule, AwesomePipe, HighlightDirective]
 })
 export class SharedModule {
 }
