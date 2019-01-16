434a3d826c827bde60e9fc31b221f713e6602bec
zongkun <nkzongkun@163.com>
2019-01-16 14:31:48 +0800

    增加rxjs部分功能

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 import { Contact, ContactService } from './contact.service';
 import { UserService } from '../core/user.service';
+import { ajax, fromPromise } from 'rxjs/internal-compatibility';
+import { catchError, debounce, debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs/internal/observable/of';
+import { interval } from 'rxjs/internal/observable/interval';
+import { Observable } from 'rxjs/internal/Observable';
+import { ActivatedRoute } from '@angular/router';
+import { fromEvent } from 'rxjs/internal/observable/fromEvent';
+import * as $ from 'jquery';
 
 @Component({
   templateUrl: './contact.component.html',
@@ -12,19 +20,60 @@ export class ContactComponent implements OnInit {
   contacts: Contact[];
   msg = 'Loading contacts ...';
   userName = '';
+  num$ = interval(1000);
+  time$ = new Observable(observer => {
+      setInterval(() => observer.next(new Date().toString()), 1000)
+    }
+  );
 
   constructor(private contactService: ContactService,
+              private activatedRoute: ActivatedRoute,
               private userService: UserService) {
     this.userName = userService.userName;
   }
 
   ngOnInit() {
+    this.activatedRoute.url.subscribe(url =>
+      console.log('The url changed to: ' + url));
+
     this.contactService.getContacts()
       .then(contacts => {
         this.msg = '';
         this.contacts = contacts;
         this.contact = contacts[0];
       });
+
+    fromPromise(fetch('assets/api/heroes.json'))
+      .subscribe(next => console.log(next));
+
+    const apiData = ajax('assets/api/heroes.json').pipe(
+      map(res => {
+        if (!res.response) {
+          throw new Error('Value expected!');
+        }
+        return res.response;
+      }),
+      catchError(err => of([]))
+    );
+
+    apiData.subscribe(next => {
+        console.log('data: ', next);
+      },
+      err => {
+        console.log('Error already caught... will not run');
+      });
+
+    const searchBox = document.getElementById('search-box');
+
+    const typeahead = fromEvent($('#search-box'), 'input').pipe(
+      map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+      filter(text => text.length > 2),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(() => ajax('assets/api/heroes.json'))
+    );
+
+    typeahead.subscribe(data => console.log(data));
   }
 
   onSubmit() {
