9e7f10f16d0857b9d638e0539042947859db83de
zongkun <nkzongkun@163.com>
2019-01-26 15:58:53 +0800

    增加动画

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HeroesModule } from './heroes/heroes.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './not-found.component';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, CoreModule, HeroesModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule, CoreModule, HeroesModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor(router: Router) {
+  }
 }
